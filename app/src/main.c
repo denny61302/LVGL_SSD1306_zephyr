1fec5056ec5d90e5694e61186304d6d790fac21d
denny61302 <denny61302@gmail.com>
2024-05-22 16:15:56 +0930

    Echo bot
    Add UART and BLE echo bot.
    
    show echo message received from BLE and UART

--- app/src/main.c
+++ app/src/main.c
@@ -1,35 +1,285 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/display.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/logging/log.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <bluetooth/services/nus.h>
+
+#include <dk_buttons_and_leds.h>
 
 #include <lvgl.h>
 
+#include <stdio.h>
+#include <string.h>
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN	(sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED DK_LED1
+#define RUN_LED_BLINK_INTERVAL 1000
+
+#define CON_STATUS_LED DK_LED2
+
+#define UART_BUF_SIZE 40
+#define UART_WAIT_FOR_BUF_DELAY K_MSEC(50)
+#define UART_WAIT_FOR_RX 50000
+
+static K_SEM_DEFINE(ble_init_ok, 0, 1);
+
+static struct bt_conn *current_conn;
+
+static const struct device *uart = DEVICE_DT_GET(DT_CHOSEN(nordic_nus_uart));
+
+#define CMD_MSG_SIZE 82
+static char rx_buf[CMD_MSG_SIZE];
+static int rx_buf_pos;
+
+/* Queue to store up to 4 messages (aligned to 4-byte boundary) */
+K_MSGQ_DEFINE(uart_msgq, CMD_MSG_SIZE, 4, 4);
+
+struct k_work uart_work;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_VAL),
+};
+
+uint32_t count = 0U;
+char count_str[11] = {0};
+const struct device *display_dev;
+lv_obj_t *hello_world_label;
+lv_obj_t *count_label;
+
 LOG_MODULE_REGISTER(main);
 
-void main(void)
+void uart_send(char *buffer)
 {
-	uint32_t count = 0U;
-	char count_str[11] = {0};
-	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
-	lv_obj_t *count_label;
+	for (int i = 0; i < strlen(buffer); i++) {
+		uart_poll_out(uart, buffer[i]);
+	}
+}
+
+void uart_cb(const struct device *dev, void *user_data)
+{
+	uint8_t c;
+
+	if (!uart_irq_update(uart)) {
+		return;
+	}
+
+	if (!uart_irq_rx_ready(uart)) {
+		return;
+	}
+
+	/* read until FIFO empty */
+	while (uart_fifo_read(uart, &c, 1) == 1) {
+		//LOG_INF("%d", c);
+		if (c == '\n') {
+
+		} else if ((c == '\r') && rx_buf_pos > 0) {
+			/* terminate string */
+			rx_buf[rx_buf_pos] = '\0';
+			//LOG_INF("[%s]",rx_buf);
+
+			/* if queue is full, message is silently dropped */
+			if (rx_buf[0] == 0) {
+				// Remove null at start of string
+				k_msgq_put(&uart_msgq, &rx_buf[1], K_NO_WAIT);
+			} else { 
+				k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+			}
+			k_work_submit(&uart_work);
+
+			/* reset the buffer (it was copied to the msgq) */
+			rx_buf_pos = 0;
+		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+			rx_buf[rx_buf_pos++] = c;
+		}
+		/* else: characters beyond buffer size are dropped */
+	}
+}
+
+void uart_work_function(struct k_work *item)
+{
+	char buffer[CMD_MSG_SIZE];
+    char uart_message[100];
+
+	//LOG_INF("ESP32/UART Work Function");
+
+	// Keep processing until there are no more NMEA messages in queue:
+	while (k_msgq_num_used_get(&uart_msgq)) {
+		// 1 or more messages in message queue:
+		if (k_msgq_get(&uart_msgq, &buffer, K_FOREVER) == 0) {
+            uart_send("Echo: ");
+		    uart_send(buffer);
+		    uart_send("\r\n");      
+            sprintf(uart_message, "UART Echo: %s", buffer); 
+            lv_label_set_text(count_label, uart_message);  
+		}
+	}
+
+}
+
+static int uart_init(void)
+{
+	int ret;
+
+	if (!device_is_ready(uart)) {
+		return -ENODEV;
+	}
+
+	ret = uart_irq_callback_user_data_set(uart, uart_cb, NULL);
+	if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			LOG_ERR("Interrupt-driven UART API support not enabled");
+		} else if (ret == -ENOSYS) {
+			LOG_ERR("UART device does not support interrupt-driven API");
+		} else {
+			LOG_ERR("Error setting UART callback: %d", ret);
+		}
+		return -1;
+	}
+
+	k_work_init(&uart_work, uart_work_function);
+
+	uart_irq_rx_enable(uart);
+
+	return ret;
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (err) {
+		LOG_ERR("Connection failed (err %u)", err);
+		return;
+	}
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("Connected %s", addr);
+
+	current_conn = bt_conn_ref(conn);
+
+	dk_set_led_on(CON_STATUS_LED);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Disconnected: %s (reason %u)", addr, reason);
+
+	if (current_conn) {
+		bt_conn_unref(current_conn);
+		current_conn = NULL;
+		dk_set_led_off(CON_STATUS_LED);
+	}
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected    = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_receive_cb(struct bt_conn *conn, const uint8_t *const data,
+			  uint16_t len)
+{
+	int err;
+	char addr[BT_ADDR_LE_STR_LEN] = {0};
+    char message[40];
+    memcpy(message, data, MIN(sizeof(message) - 1, len));
+    message[MIN(sizeof(message) - 1, len)] = '\0';
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, ARRAY_SIZE(addr));
+
+	LOG_INF("Received data from: %s", addr);	
+    LOG_INF("Received data: %s", message);
+    LOG_INF("Received data length: %d", len);
+
+    char ble_message[100];
+    
+    int packet_len = sprintf(ble_message, "BLE Echo: %s", message);
+    bt_nus_send(NULL, ble_message, packet_len);
+	lv_label_set_text(count_label, ble_message);
+}
+
+static struct bt_nus_cb nus_cb = {
+	.received = bt_receive_cb,
+};
+
+static void configure_gpio(void)
+{
+	int err;
+
+	err = dk_leds_init();
+	if (err) {
+		LOG_ERR("Cannot init LEDs (err: %d)", err);
+	}
+}
+
+void error(void)
+{
+	dk_set_leds_state(DK_ALL_LEDS_MSK, DK_NO_LEDS_MSK);
+
+	while (true) {
+		/* Spin for ever */
+		k_sleep(K_MSEC(1000));
+	}
+}
+
+int main(void)
+{
+	int blink_status = 0;
+    int err = 0;
+
+    configure_gpio();
+
+	err = uart_init();
+	if (err) {
+		error();
+	}
+    
+    err = bt_enable(NULL);
+	if (err) {
+		error();
+	}
+
+    k_sem_give(&ble_init_ok);
+
+    err = bt_nus_init(&nus_cb);
+	if (err) {
+		LOG_ERR("Failed to initialize UART service (err: %d)", err);
+	}
+
+    err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd,
+			      ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+	}  
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 
 	if (display_dev == NULL) {
-		return;
+        LOG_ERR("Display device not found. Aborting application.");
 	}
 
-	if (IS_ENABLED(CONFIG_LVGL_POINTER_KSCAN)) {
-		lv_obj_t *hello_world_button;
-
-		hello_world_button = lv_btn_create(lv_scr_act());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
-		hello_world_label = lv_label_create(hello_world_button);
-	} else {
-		hello_world_label = lv_label_create(lv_scr_act());
-	}
-
+	hello_world_label = lv_label_create(lv_scr_act());
+	
 	lv_label_set_text(hello_world_label, "Hello world!");
 	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
@@ -39,13 +289,9 @@ void main(void)
 	lv_task_handler();
 	display_blanking_off(display_dev);
 
-	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count/100U);
-			lv_label_set_text(count_label, count_str);
-		}
+	while (1) {		
 		lv_task_handler();
-		k_sleep(K_MSEC(10));
-		++count;
+		k_sleep(K_MSEC(100));
+        dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
 	}
 }
